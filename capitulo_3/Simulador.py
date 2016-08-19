107f6f33c59e5c298ea0451ef84e4a808f0896fd
AlexMtz <yamop.16@gmail.com>
2016-08-19 02:19:18 -0500

    ADD comments and UPDATE the interface

--- capitulo_3/Simulador.py
+++ capitulo_3/Simulador.py
@@ -1,5 +1,77 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Archivo: Simulador.py
+# Capitulo: 3 Estilo Publica-Subscribe
+# Autor(es): Perla Velasco & Yonathan Mtz.
+# Version: 1.2 Agosto 2016
+# Descripción:
+#
+#   Ésta clase define el rol de set-up del proyecto y permite instanciar las clases para
+#   construir un entorno simulado del caso de estudio.
+#
+#   Las características de ésta clase son las siguientes:
+#
+#                                        Simulador.py
+#           +-----------------------+-------------------------+------------------------+
+#           |  Nombre del elemento  |     Responsabilidad     |      Propiedades       |
+#           +-----------------------+-------------------------+------------------------+
+#           |                       |                         |  - Inicializa los pu-  |
+#           |                       |                         |    blicadores y los    |
+#           |                       |                         |    subscriptores.      |
+#           |        Set-Up         |  - Levantar entorno de  |  - Solicita al usuario |
+#           |                       |    simulación.          |    las características |
+#           |                       |                         |    con las que contará |
+#           |                       |                         |    la simulación a eje-|
+#           |                       |                         |    cutar.              |
+#           +-----------------------+-------------------------+------------------------+
+#
+#   A continuación se describen los métodos que se implementaron en ésta clase:
+#
+#                                             Métodos:
+#           +-------------------------+--------------------------+-----------------------+
+#           |         Nombre          |        Parámetros        |        Función        |
+#           +-------------------------+--------------------------+-----------------------+
+#           |                         |                          |  - Solicita al usua-  |
+#           |                         |                          |    rio los valores    |
+#           |                         |                          |    necesarios para i- |
+#           |                         |                          |    nicializar la si-  |
+#           |                         |                          |    mulación.          |
+#           |         main()          |           None           |  - Manda crear los    |
+#           |                         |                          |    publicadores.      |
+#           |                         |                          |  - Manda crear los    |
+#           |                         |                          |    subscriptores.     |
+#           |                         |                          |  - Solicita los valo- |
+#           |                         |                          |    res máximos que    |
+#           |                         |                          |    generarán los      |
+#           |                         |                          |    eventos.           |
+#           +-------------------------+--------------------------+-----------------------+
+#           |   create_temperature    |     String: nombre       |  - Instancia la clase |
+#           |        _sensor()        |                          |    SensorTemperatura. |
+#           +-------------------------+--------------------------+-----------------------+
+#           |     create_preasure     |     String: nombre       |  - Instancia la clase |
+#           |        _sensor()        |                          |    SensorPresion.     |
+#           +-------------------------+--------------------------+-----------------------+
+#           |    create_heart_rate    |     String: nombre       |  - Instancia la clase |
+#           |        _sensor()        |                          |    SensorRitmoCardiaco|
+#           +-------------------------+--------------------------+-----------------------+
+#           |                         |                          |  - Ejecuta los metodos|
+#           |    run_simulator()      |           None           |    que inician a los  |
+#           |                         |                          |    subscriptores y a  |
+#           |                         |                          |    los publicadores.  |
+#           +-------------------------+--------------------------+-----------------------+
+#           |                         |                          |  - Abre tres termina- |
+#           |    start_consumers()    |           None           |    les y en cada una  |
+#           |                         |                          |    de ellas ejecuta un|
+#           |                         |                          |    subscriptor.       |
+#           +-------------------------+--------------------------+-----------------------+
+#           |                         |                          |  - Simula 1000 eventos|
+#           |    start_publishers()   |           None           |    generados por los  |
+#           |                         |                          |    sensores.          |
+#           +-------------------------+--------------------------+-----------------------+
+#
+#--------------------------------------------------------------------------------------------------
+
 import os
 import time
 import pika
@@ -9,49 +81,104 @@ from SensorPresion import SensorPresion
 
 
 class SetUpSimulador:
-    """
-    Esta clase permite Simular los eventos en un asilo de adultos
-    mayores.
-    """
     sensores = []
     temperatura = 0
     ritmo_cardiaco = 0
     presion = 0
 
     def main(self):
-        print('Bienvenido al Simulador P/S versión 1.0')
-        print('Simularemos 3 tipos de eventos en adultos mayores:')
-        print('Calentura, Hipertensión y Taquicardia')
-        print("¿Cuántos publicadores?")
+        print('+---------------------------------------------+')
+        print('|  Bienvenido al Simulador Publica-Subscribe  |')
+        print('+---------------------------------------------+')
+        print('')
+        raw_input('presiona enter para continuar: ')
+        print('')
+        print('+---------------------------------------------+')
+        print('|        Evento        |      Descripción     |')
+        print('+---------------------------------------------+')
+        print('|                      |   - La temperatura   |')
+        print('|       Calentura      |     corporal supera  |')
+        print('|                      |     los 38°.         |')
+        print('+----------------------+----------------------+')
+        print('|                      |   - Presión excesiva-|')
+        print('|                      |     mente alta de la |')
+        print('|     Hipertensión     |     sangre sobre la  |')
+        print('|                      |     pared de las ar- |')
+        print('|                      |     terias.          |')
+        print('+----------------------+----------------------+')
+        print('|     Taquicardia      |   - Velocidad excesi-|')
+        print('|                      |     va del ritmo de  |')
+        print('|                      |     los latidos del  |')
+        print('|                      |     corazón.         |')
+        print('+----------------------+----------------------+')
+        print('')
+        raw_input('presiona enter para continuar: ')
+        print('')
+        print('+---------------------------------------------+')
+        print('|        CONFIGURACIÓN DE LA SIMULACIÓN       |')
+        print('+---------------------------------------------+')
+        print('|        Número de publicadores         |  ?  |')
+        print('+---------------------------------------------+')
         publishers = raw_input('número entero: ')
-        print("Configuremos cada uno de los publicadores")
+        print('+---------------------------------------------+')
+        print('|        Número de publicadores         |  ' + publishers +  '  |')
+        print('+---------------------------------------------+')
+        print('|            ASIGNACIÓN DE SENSORES           |')
+        print('+---------------------------------------------+')
+        print('')
+        raw_input('presiona enter para continuar: ')
+        print('')
         for x in xrange(0, int(publishers)):
-            print("¿A quién se asociara éste publicador (Sensor)?")
-            nombre = raw_input('nombre de la persona adulta: ')
+            print('+---------------------------------------------+')
+            print('|            DATOS DEL ADULTO MAYOR |    ' + str(x) +  '    |')
+            print('+---------------------------------------------+')
+            print('|           NOMBRE           |        ?       |')
+            print('+---------------------------------------------+')
+            nombre = raw_input('escribe el nombre: ')
+            print('+---------------------------------------------+')
+            print('|           NOMBRE           | ' + nombre +' |')
+            print('+---------------------------------------------+')
             self.create_temperature_sensor(nombre)
+            print('|     SENSOR TEMPERATURA     |    ASIGNADO   |')
+            print('+---------------------------------------------+')
             self.create_preasure_sensor(nombre)
+            print('|       SENSOR PRESIÓN       |    ASIGNADO   |')
+            print('+---------------------------------------------+')
             self.create_heart_rate_sensor(nombre)
-        print("Para los consumidores, tendremos un monitor que nos indicará cuando un evento suceda")
-        print("Para ello configuraremos nuestro monitor")
-        print("**TEMPERATURA**")
-        print('Para detectar calentura es necesario establecer')
-        print('(Se recomienda utilizar un valor mínimo de 37)')
-        print('¿Cuál será la temperatura máxima soportada por los adultos?')
+            print('|    SENSOR RITMO CARDIACO   |    ASIGNADO   |')
+            print('+---------------------------------------------+')
+            print('')
+            raw_input('presiona enter para continuar: ')
+        print('+---------------------------------------------+')
+        print('|        VALORES MÁXIMOS DE LOS EVENTOS       |')
+        print('+---------------------------------------------+')
+        print('|          TEMPERATURA       |        ?       |')
+        print('+---------------------------------------------+')
         temperatura_maxima = raw_input('temperatura máxima: ')
         self.temperatura = int(temperatura_maxima)
-        print("**RITMO CARDIACO**")
-        print('Para detectar Taquicardia es necesario establecer')
-        print('(Se recomienda utilizar un valor mínimo de 100)')
-        print('¿Cuántos serán los latidos máximos permitidos en los adultos?')
+        print('+---------------------------------------------+')
+        print('|          TEMPERATURA       |       ' + temperatura_maxima + '       |')
+        print('+---------------------------------------------+')
+        print('|       RITMO CARDIACO       |        ?       |')
+        print('+---------------------------------------------+')
         ritmo_maximo = raw_input('ritmo máximo: ')
         self.ritmo_cardiaco = int(ritmo_maximo)
-        print("**PRESION ARTERIAL**")
-        print('Para detectar Hipertensión es necesario establecer')
-        print('(Se recomienda utilizar un valor mínimo de 140)')
-        print('¿Cuál es la presión máxima soportada por los adultos?')
+        print('+---------------------------------------------+')
+        print('|       RITMO CARDIACO       |      ' + ritmo_maximo +'       |')
+        print('+---------------------------------------------+')
+        print('|      PRESION ARTERIAL      |        ?       |')
+        print('+---------------------------------------------+')
         presion_maxima = raw_input('presión máxima: ')
         self.presion = int(presion_maxima)
-        print("Iniciando consumidores listo para escuchar")
+        print('+---------------------------------------------+')
+        print('|      PRESION ARTERIAL      |      ' + presion_maxima + '       |')
+        print('+---------------------------------------------+')
+        print('|   CONFIGURACIÓN DE LA SIMULACIÓN TERMINADA  |')
+        print('+---------------------------------------------+')
+        raw_input('presiona enter para continuar: ')
+        print('+---------------------------------------------+')
+        print('|             INICIANDO SIMULACIÓN            |')
+        print('+---------------------------------------------+')
         self.run_simulator()
 
     def create_temperature_sensor(self, nombre):
@@ -83,10 +210,6 @@ class SetUpSimulador:
             for s in self.sensores:
                 s.start_service()
                 time.sleep(1.0)
-
-    def stop_simulation(self):
-        for s in self.sensores:
-            s.stop()
 
 if __name__ == '__main__':
     simulador = SetUpSimulador()
